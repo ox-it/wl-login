9e62f2062f4101b476284d9dacffaff168ca96e8
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2009-07-24 14:35:48 +0100

    WL-747 Added a Webauth specific timeout on cookie.

--- login-tool/tool/src/java/org/sakaiproject/login/tool/CookieFilter.java
+++ login-tool/tool/src/java/org/sakaiproject/login/tool/CookieFilter.java
@@ -34,6 +34,7 @@ public class CookieFilter implements Filter {
 	
 	private boolean enabled;
 	private boolean remove;
+	private boolean always;
 
 	public void destroy() {
 
@@ -44,24 +45,14 @@ public class CookieFilter implements Filter {
 		chain.doFilter(request, response);
 		if (enabled) {
 			if (response.isCommitted()) {
-				log.warn("The response has already been committed, we can set a cookie.");
+				log.warn("The response has already been committed, we can't set a cookie.");
 			}
 			if (request instanceof HttpServletRequest && response instanceof HttpServletResponse) {
 				HttpServletRequest httpRequest = (HttpServletRequest)request;
 				HttpServletResponse httpResponse = (HttpServletResponse)response;
-				Cookie[] cookies = httpRequest.getCookies();
-				boolean hasCookie = false;
-				if (cookies != null) {
-					for (Cookie cookie: cookies) {
-						if (cookieName.equals(cookie.getName())) {
-							hasCookie = true;
-							break;
-						}
-					}
-				}
 
-				if (hasCookie == remove) {
-					Cookie cookie = new Cookie(cookieName, cookieValue);
+				if (always || hasCookie(httpRequest) == remove) {
+					Cookie cookie = new Cookie(cookieName, getCookieValue(httpRequest));
 					cookie.setSecure(cookieSecure);
 					cookie.setMaxAge(cookieAge);
 					if (cookiePath != null) {
@@ -73,12 +64,27 @@ public class CookieFilter implements Filter {
 		}
 	}
 
+	private boolean hasCookie(HttpServletRequest httpRequest) {
+		Cookie[] cookies = httpRequest.getCookies();
+		boolean hasCookie = false;
+		if (cookies != null) {
+			for (Cookie cookie: cookies) {
+				if (cookieName.equals(cookie.getName())) {
+					hasCookie = true;
+					break;
+				}
+			}
+		}
+		return hasCookie;
+	}
+
 	public void init(FilterConfig filterConfig) throws ServletException {
 		cookieName = filterConfig.getInitParameter("name");
 		cookieValue = filterConfig.getInitParameter("value");
 		cookiePath = filterConfig.getInitParameter("path");
 		cookieSecure = Boolean.valueOf(filterConfig.getInitParameter("secure")).booleanValue();
 		remove = Boolean.valueOf(filterConfig.getInitParameter("remove")).booleanValue();
+		always = Boolean.valueOf(filterConfig.getInitParameter("always")).booleanValue();
 		cookieAge = (remove)?0:-1;
 		
 		if (cookieName != null && cookieName.length() > 0 &&
@@ -88,7 +94,10 @@ public class CookieFilter implements Filter {
 		} else {
 			log.error("You need to set name/value init parameters for this filter to work.");
 		}
-
+	}
+	
+	public String getCookieValue(HttpServletRequest request) {
+		return cookieValue;
 	}
 
 }
---
+++ login-tool/tool/src/java/org/sakaiproject/login/tool/WACookieFilter.java
@@ -0,0 +1,81 @@
+package org.sakaiproject.login.tool;
+
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+
+/**
+ * Attempt to work out when the Webauth session expires.
+ * @author buckett
+ *
+ */
+public class WACookieFilter extends CookieFilter {
+
+	private static final Log log = LogFactory.getLog(WACookieFilter.class);
+	
+	// Default of 8 hours.
+	private int defaultDuration = 1000 * 60 * 60 * 8;
+	
+	// Minimum duration of 30 minutes.
+	private int minimumDuration = 1000 * 60 * 30;
+
+	// Request parameter to pull the expiry time from.
+	private static String requestParam = "WEBAUTH_TOKEN_EXPIRATION";
+
+	
+	public void init(FilterConfig config) throws ServletException {
+		super.init(config);
+		String param = null;
+		
+		param = config.getInitParameter("requestParam");
+		if (param != null && param.length() > 0) {
+			requestParam = (String)param;
+		}
+		
+		try {
+			param = config.getInitParameter("defaultDuration");
+			if (param != null && param.length() > 0) {
+				defaultDuration = Integer.parseInt(param);
+			}
+		} catch (NumberFormatException nfe) {
+			log.warn("defaultDuration is not a number: "+ param);
+		}
+		try {
+			param = config.getInitParameter("minimumDuration");
+			if (param != null && param.length() > 0) {
+				minimumDuration = Integer.parseInt(param);
+			}
+		} catch (NumberFormatException nfe) {
+			log.warn("minimumDuration is not a number: "+ param);
+		}
+	}
+	
+	public String getCookieValue(HttpServletRequest request) {
+		Object attr = request.getAttribute(requestParam);
+		long now = System.currentTimeMillis();
+		long expiryTime = now + minimumDuration;
+		if (attr != null && attr instanceof String) {
+			String attrString = (String)attr;
+			try {
+				long expiryTimeWA = Long.parseLong(attrString);
+				if (expiryTimeWA < now) {
+					log.warn("WebAuth session has apparently already expired. Time: "+ now+ " Expired: "+ expiryTime);
+				} else if (expiryTimeWA < now + minimumDuration) {
+					log.debug("Webauth session expires too quickly, giving user a little grace.");
+				} else {
+					expiryTime = expiryTimeWA;
+				}
+			} catch (NumberFormatException nfe) {
+				log.info("WebAuth expiry isn't a number: "+ attrString);
+			}
+		} else {
+			log.warn("Request attribute isn't good: "+ attr);
+			expiryTime = now + defaultDuration;
+		}
+		return Long.toString(expiryTime);
+	}
+}
