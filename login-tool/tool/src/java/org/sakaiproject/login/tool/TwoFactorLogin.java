95e622611dded8b7d6e8adb17a4e40aec519bd2b
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2011-07-21 11:37:24 +0100

    WL-1621 Fix for the username suffix being incorrect.

--- login-tool/tool/src/java/org/sakaiproject/login/tool/TwoFactorLogin.java
+++ login-tool/tool/src/java/org/sakaiproject/login/tool/TwoFactorLogin.java
@@ -73,7 +73,7 @@ public class TwoFactorLogin extends HttpServlet
 
 	private TwoFactorAuthentication twoFactorAuthentication;
 
-	private String usernameSuffix = "@OX.AC.UK";
+	private String usernameSuffix = "@ox.ac.uk";
 
 	private long gracePeriod = 5000; // 5 seconds
 
@@ -191,6 +191,7 @@ public class TwoFactorLogin extends HttpServlet
 		}
 		String aid = remoteUser.replaceFirst(usernameSuffix, "");
 		if (remoteUser.equals(aid)) {
+			M_log.warn("Bad username of: "+ remoteUser);
 			throw new RuntimeException("Cannot handle your username, it should end with "+ usernameSuffix);
 		}
 		
